348a8de307537497f331b7a8b19169a59f36b981
Nisha-Mani <nishamani195@gmail.com>
2025-05-06 00:02:05 +0530

    Feature: displaying results with search bar and filter status

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,52 @@ import "./App.css";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
-
+  const [filter, setFilter] = useState("All");
+  console.log(filter);
   const jobs = [
-    { key: 1, company: "Google", title: "Frontend Developer" },
-    { key: 2, company: "Amazon", title: "Backend Developer" },
-    { key: 3, company: "Microsoft", title: "Full Stack Engineer" },
-    { key: 4, company: "Netflix", title: "UI/UX Designer" },
+    {
+      key: 1,
+      company: "Google",
+      title: "Frontend Developer",
+      status: "Applied",
+    },
+    {
+      key: 2,
+      company: "Amazon",
+      title: "Backend Developer",
+      status: "Rejected",
+    },
+    {
+      key: 3,
+      company: "Microsoft",
+      title: "Full Stack Engineer",
+      status: "Applied",
+    },
+    { key: 4, company: "Netflix", title: "UI/UX Designer", status: "Rejected" },
+    {
+      key: 5,
+      company: "Google Cloud",
+      title: "Cloud Developer",
+      status: "Rejected",
+    },
   ];
-  const filteredJobs = searchTerm
-    ? jobs.filter((job) => {
-        return (
-          job.company.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          job.title.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-      })
-    : jobs;
 
-  console.log(filteredJobs);
-  console.log(searchTerm);
+  const filteredJobs = jobs.filter((job) => {
+    const matchesSearch =
+      job.company.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      job.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchesStatus = filter === "All" || job.status === filter;
+
+    return matchesSearch && matchesStatus;
+  });
+
   return (
     <>
       <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <button>Add New Job</button>
 
       <label htmlFor="status">Filter by Status:</label>
-      <select id="status">
+      <select id="status" onChange={(e) => setFilter(e.target.value)}>
         <option>All</option>
         <option>Applied</option>
         <option>Rejected</option>
